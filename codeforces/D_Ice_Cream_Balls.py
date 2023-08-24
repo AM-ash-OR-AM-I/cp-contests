4363ad45cb308167314ad55d5f19fc1db0664f33
Ashutosh Mahapatra <ashutosh2gudu@gmail.com>
2023-08-24 23:49:19 +0530

    Updated code

--- codeforces/D_Ice_Cream_Balls.py
+++ codeforces/D_Ice_Cream_Balls.py
@@ -2,12 +2,10 @@ ints = lambda: list(map(int, input().split()))
 alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 mx = 10**5
 inp = lambda: int(input())
+from math import comb, isqrt
 
 for _ in range(inp()):
     n = inp()
-    x = (1 + (1+8*n)**.5)/2
-    if x != int(x):
-        x = int(x)
-        print(int(x+(n-(x*(x-1)/2))))
-    else:
-        print(int(x))
+    x = int((1 + isqrt(1+8*n))/2)
+    x += n - comb(x, 2)
+    print(x)
